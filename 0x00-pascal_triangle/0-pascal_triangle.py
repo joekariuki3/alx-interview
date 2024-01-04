4b8591cc414a6f41e261028753b5c8f49eb6681a
joekariuki3 <joekariuki3@gmail.com>
2024-01-04 22:13:17 +0300

    pascal tringle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# function that return pascal triangle of n. n is 0 or less returns empty list
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
